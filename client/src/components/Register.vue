a0062d559c8a76fd37e73ddd91d78ed1d36cab9f
Qarton <mathieumarkides@gmail.com>
2020-12-15 17:50:30 +0100

    début page de login

---
+++ client/src/components/Register.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1> register </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
